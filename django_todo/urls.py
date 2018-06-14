b8f3a025b9d1618f8883c1acf36c95ef9ffd4128
Chris Zielinski <ckz8780@gmail.com>
2018-06-14 23:47:07 +0000

    added toggle status buttons and functionality

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list, create_an_item, edit_an_item
+from todo.views import get_todo_list, create_an_item, edit_an_item, toggle_status
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_todo_list),
     path('add/', create_an_item),
-    path('edit/<int:id>/', edit_an_item)
+    path('edit/<int:id>/', edit_an_item),
+    path('toggle/<int:id>/', toggle_status),
 ]
