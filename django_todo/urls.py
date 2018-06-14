0e67c6bdc129898e1bcb8f1bef727f4103838af0
Chris Zielinski <ckz8780@gmail.com>
2018-06-14 23:36:45 +0000

    Added edit item template and routing

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list, create_an_item
+from todo.views import get_todo_list, create_an_item, edit_an_item
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_todo_list),
-    path('add', create_an_item), 
+    path('add', create_an_item),
+    path('edit/<int:id>', edit_an_item)
 ]
