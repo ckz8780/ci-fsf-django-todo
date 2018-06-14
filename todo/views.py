652c4f7a375ec6856906a56eef7ec346d6f2f9fa
Chris Zielinski <ckz8780@gmail.com>
2018-06-14 23:14:02 +0000

    added add an item template and url

--- todo/views.py
+++ todo/views.py
@@ -4,3 +4,7 @@ from .models import Item
 def get_todo_list(request):
     results = Item.objects.all()
     return render(request, "todo_list.html", {'items' : results})
+    
+    
+def create_an_item(request):
+    return render(request, "item_form.html")
